44f36beb0b7d2e6eba9c9dca680ef6ad93b826f6
Victor Massé <victor.dhamma@gmail.com>
2022-05-28 09:53:36 +0200

    add prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [
-    require('@tailwindcss/typography')
-  ],
+  important: true,
+  corePlugins: {
+    preflight: false,
+  },
+	plugins: [],
 }
