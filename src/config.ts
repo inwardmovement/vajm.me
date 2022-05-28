3b48397826ed6f3cbd64648fb11833f34a035594
Victor Massé <victor.dhamma@gmail.com>
2022-05-28 17:29:06 +0200

    use netlify git variable

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,5 @@
-import git from "git-repo-info"
-
 // prettier-ignore
 export default {
   title: "vajm.me",
-  description: "Poèmes",
-  version: git().abbreviatedSha.substring(0, 7)
-    + " "
-    + git().commitMessage
+  description: "Poèmes"
 }
