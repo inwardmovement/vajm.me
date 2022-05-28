3041e61d6fbe2a1a0327a01a0cd3f46888c720e8
Victor Massé <victor.dhamma@gmail.com>
2022-05-28 16:20:04 +0200

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+// prettier-ignore
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {}
-  },
+  prefix: "tw-",
   important: true,
   corePlugins: {
     preflight: false
   },
-  plugins: []
+  plugins: [],
+  theme: {
+    screens: {
+      "xs": "0",
+      "sm": "576px",
+      "md": "768px",
+      "lg": "992px",
+      "xl": "1200px",
+      "2xl": "1400px"
+    },
+    extend: {}
+  }
 }
