21c7af2ebd3b3b25ce0c05fdaca9a7d885243096
Victor Massé <victor.dhamma@gmail.com>
2022-05-29 09:37:30 +0200

    test env variables

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,10 @@
 export default {
   title: "vajm.me",
   description: "Poèmes",
-  netlify: import.meta.env.NETLIFY,
-  version: import.meta.env.NETLIFY && (import.meta.env.COMMIT_REF).substring(0, 7)
+  dev:            import.meta.env.DEV,
+  staging:        import.meta.env.NETLIFY && (import.meta.env.CONTEXT == ("deploy-preview" || "branch-deploy")),
+  prod:           import.meta.env.NETLIFY && (import.meta.env.CONTEXT == "production"),
+  prodOrStaging:  import.meta.env.NETLIFY && (import.meta.env.CONTEXT == ("production" || "deploy-preview" || "branch-deploy")),
+  version:        import.meta.env.NETLIFY && (import.meta.env.COMMIT_REF).substring(0, 7),
+  context:        import.meta.env.NETLIFY && import.meta.env.CONTEXT
 }
