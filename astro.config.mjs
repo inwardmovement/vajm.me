861076b5bd90903450a539d427cf83db389c270c
Victor Massé <victor.dhamma@gmail.com>
2022-05-30 19:24:51 +0200

    [skip ci]

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import vue from "@astrojs/vue"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vajm.me",
   integrations: [tailwind(), vue()]
-});
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   plugins: [
-    require('@headlessui/tailwindcss'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms')
+    require("@headlessui/tailwindcss"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms")
   ],
   theme: {
     extend: {}
