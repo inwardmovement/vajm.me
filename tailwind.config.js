4420b3ecbbc11ed9e04a9da4819dd4ae42aed6e8
Victor Massé <victor.dhamma@gmail.com>
2022-05-30 18:19:49 +0200

    add headlessui/tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   plugins: [
+    require('@headlessui/tailwindcss'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms')
   ],
