5140e714a0f91cd0b737ce6b626e84d5bd49654d
Victor Massé <victor.dhamma@gmail.com>
2022-05-30 18:54:25 +0200

    add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://vajm.me",
-  integrations: [tailwind()]
-})
+  integrations: [tailwind(), vue()]
+});
