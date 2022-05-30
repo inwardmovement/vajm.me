c0a485e0a9a3b723254b06d675ed05e2ef528511
Victor Massé <victor.dhamma@gmail.com>
2022-05-30 20:14:58 +0200

    add alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
-import vue from "@astrojs/vue"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vajm.me",
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind()]
 })
