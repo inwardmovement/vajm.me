3adf66c462986581fcff3d095de8495219156708
Victor Massé <victor.dhamma@gmail.com>
2022-05-30 19:58:05 +0200

    [skip ci] remove vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-  plugins: [
-    require("@headlessui/tailwindcss"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms")
-  ],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
   theme: {
     extend: {}
   }
