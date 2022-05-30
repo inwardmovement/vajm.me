3396ad20bac3cc610940249f5e6df0750f74c16c
Victor Massé <victor.dhamma@gmail.com>
2022-05-30 07:36:33 +0200

    remove bootstrap

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,7 @@
-// prettier-ignore
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-  prefix: "tw-",
-  important: true,
-  corePlugins: {
-    preflight: false
-  },
   plugins: [],
   theme: {
-    screens: {
-      "xs": "0",
-      "sm": "576px",
-      "md": "768px",
-      "lg": "992px",
-      "xl": "1200px",
-      "2xl": "1400px"
-    },
     extend: {}
   }
 }
