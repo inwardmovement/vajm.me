4a47c220cd5823ebe5a324f1b0778e18d56fe50e
Victor Massé <victor.dhamma@gmail.com>
2022-05-30 12:37:33 +0200

    add tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms')
+  ],
   theme: {
     extend: {}
   }
