2d112e5005069b6e59c1fe100c4dfa93da8a974b
Victor Massé <victor.dhamma@gmail.com>
2022-06-02 22:16:53 +0200

    Revert "adapt tailwind to bootsrap"
    This reverts commit e794c0255119d63804f4757adb85b675ff2c6a57.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,7 @@
-// prettier-ignore
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-  prefix: 'tw-',
-  important: true,
-  corePlugins: {
-    preflight: false,
-  },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
   theme: {
-    screens: {
-      'sm': '576px',
-      'md': '768px',
-      'lg': '992px',
-      'xl': '1200px',
-      '2xl': '1400px',
-    },
     extend: {}
   }
 }
