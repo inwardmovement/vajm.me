ababc3421434fd2fc90d7fffa7540c23750c13f1
Victor Massé <victor.dhamma@gmail.com>
2022-07-16 08:51:39 +0200

    remove astro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{html,js,jsx,md,svelte,ts,tsx,vue}"],
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
